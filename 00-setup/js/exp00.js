60526ee8476fa869dcdebb64d6c6b8db48b5d0fc
enricllagostera <enricllagostera@gmail.com>
2019-10-22 21:43:21 -0400

    Initial commit with one experiment

---
+++ 00-setup/js/exp00.js
@@ -0,0 +1,30 @@
+/* 
+EXP 00
+A small experiment that paints some nice red textures based on mouse coordinates on the screen.
+*/
+
+let brushColor = {};
+let sizeMultiplier = 0.1;
+
+function setup()
+{
+    createCanvas(windowWidth - 50, windowHeight - 50);
+    background(0);
+    noStroke();
+    brushColor = color(255, 0, 0);
+}
+
+function draw()
+{
+    if (mouseIsPressed)
+    {
+        brushColor = color(0, 0, 0);
+    }
+    else
+    {
+        brushColor = color(255, 0, 0);
+    }
+    brushColor.setAlpha(random() * 255);
+    fill(brushColor);
+    rect(random() * width, random() * height, mouseX * sizeMultiplier, mouseY * sizeMultiplier);
+}
